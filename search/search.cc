9320ac457e0c5921ed5b41187232ba20b9a14957
Mike Maxim <mike.maxim@gmail.com>
2026-09-20 15:22:44 -0400

    Fix null move heuristic depth underflow causing search hang beyond depth 2

--- search/search.cc
+++ search/search.cc
@@ -33,11 +33,11 @@ int Search<Evaluator>::negamax_internal(Board& b, int depth, int alpha, int beta
     return evaluator_.evaluate(b);
   }
 
-  if (can_null && depth >= 2 && !in_check(b)) {
+  const int R = 2;
+  if (can_null && depth > R && !in_check(b)) {
     auto saved_side = b.side_to_move;
     b.side_to_move = opponent(saved_side);
-    auto R = 2;
-    auto score = -negamax_internal(b, depth - R - 1, -beta, -beta + 1, false);
+    auto score = -negamax_internal(b, depth - R, -beta, -beta + 1, false);
     b.side_to_move = saved_side;
     if (score >= beta) {
       return beta;
